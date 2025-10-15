0ccdb62b39668d47dcd3710f1fdad4b396a97dad
Maximilian Combüchen <max.combuchen@snyk.io>
2025-10-15 10:51:48 +0200

    feat: add package legacypolicy for .snyk support

---
+++ pkg/legacypolicy/policy.go
@@ -0,0 +1,90 @@
+package legacypolicy
+
+import (
+	"fmt"
+	"io"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+const specVersion = "v1.25.1"
+
+// New returns a pointer to a Policy, which will be prepopulated with a version
+// and non-nil maps. This is the preferred way to create a new policy from scratch.
+func New() *Policy {
+	return &Policy{
+		Version: specVersion,
+		Ignore:  make(RuleSet),
+	}
+}
+
+// Unmarshal reads a policy from r into target.
+func Unmarshal(r io.Reader, target *Policy) error {
+	if err := yaml.NewDecoder(r).Decode(target); err != nil {
+		return fmt.Errorf("failed to decode snyk policy: %w", err)
+	}
+	return nil
+}
+
+// Marshal writes a serialized policy to w.
+func Marshal(w io.Writer, p *Policy) error {
+	if err := yaml.NewEncoder(w).Encode(p); err != nil {
+		return fmt.Errorf("failed to encode snyk policy: %w", err)
+	}
+	return nil
+}
+
+// Policy models the legacy .snyk policy.
+type Policy struct {
+	Version       string    `yaml:"version"`
+	FailThreshold *Severity `yaml:"failThreshold,omitempty"`
+	Ignore        RuleSet   `yaml:"ignore,omitempty"`
+}
+
+// Severity models an issues severity level.
+type Severity string
+
+const (
+	//nolint:revive // Severity levels are self-explanatory.
+	SeverityLow      = Severity("low")
+	SeverityMedium   = Severity("medium")
+	SeverityHigh     = Severity("high")
+	SeverityCritical = Severity("critical")
+)
+
+// VulnID models the unique identifier of a Snyk vulnerability.
+type VulnID string
+
+// RuleSet models rules grouped by vulnerability identifiers.
+type RuleSet map[VulnID][]RuleEntry
+
+// RuleEntry models rules grouped by the dependency path.
+type RuleEntry map[string]*Rule
+
+// Rule models an actual policy rule.
+type Rule struct {
+	Created   time.Time `yaml:"created,omitempty"`
+	Expires   time.Time `yaml:"expires,omitempty"`
+	IgnoredBy struct {
+		Email string `yaml:"email,omitempty"`
+		Name  string `yaml:"name,omitempty"`
+	} `yaml:"ignoredBy,omitempty"`
+	Reason             string     `yaml:"reason,omitempty"`
+	ReasonType         ReasonType `yaml:"reasonType,omitempty"`
+	Source             string     `yaml:"source,omitempty"`
+	From               string     `yaml:"from,omitempty"`
+	DisregardIfFixable bool       `yaml:"disregardIfFixable,omitempty"`
+}
+
+// ReasonType is an enum of known categories for why a rule was applied.
+type ReasonType string
+
+const (
+	// ReasonTypeNotVulnerable applies if an issue does not apply.
+	ReasonTypeNotVulnerable = ReasonType("not-vulnerable")
+	// ReasonTypeWontFix applies if an issue is intentionally being ignored.
+	ReasonTypeWontFix = ReasonType("wont-fix")
+	// ReasonTypeTemporaryIgnore applies if an issue is being ignored temporarily.
+	ReasonTypeTemporaryIgnore = ReasonType("temporary-ignore")
+)
