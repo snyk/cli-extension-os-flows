c45f630c5e3bece5f50e1e2579c475bb8fd6a176
Jacek Rzeniewicz <jacek@rzrz.pl>
2026-05-21 10:50:48 +0100

    test(reachability/sources): empty dir baseline

--- internal/reachability/sources/sources_test.go
+++ internal/reachability/sources/sources_test.go
@@ -1,1 +1,26 @@
 package sources_test
+
+import (
+	"io"
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"github.com/snyk/cli-extension-os-flows/internal/reachability/sources"
+)
+
+func nopLogger() *zerolog.Logger {
+	l := zerolog.New(io.Discard)
+	return &l
+}
+
+func TestHasSupportedSources_EmptyDir_ReturnsFalse(t *testing.T) {
+	dir := t.TempDir()
+
+	got, err := sources.HasSupportedSources(dir, nopLogger())
+
+	require.NoError(t, err)
+	assert.False(t, got)
+}
